e182c6d82fad8d8e1dec707484922ae7283bf62a
TVA018 <thomas.vu018@gmail.com>
2026-02-19 13:14:57 -0500

    February 19, 2026 V2

--- Calculator_And_String_Analyzer/ColourFormatter.java
+++ Calculator_And_String_Analyzer/ColourFormatter.java
@@ -1,4 +1,7 @@
 public class ColourFormatter {
+    /**
+     * ANSI CODES FOR COLOURS
+    */
     public enum ANSIColour {
         RESET("0m"),
         BLACK("30m"),
@@ -17,18 +20,41 @@ public class ColourFormatter {
         }
     }
 
+    /**
+     * Prints a string with a given colour
+     * @param colour The colour to use
+     * @param str The string to print
+     */
     public static void print(ANSIColour colour, String str) {
         System.out.print(colourString(colour, str));
     }
 
+    /**
+     * Prints a string and end with a new line using a given colour
+     * @param colour The colour to use
+     * @param str The string to print
+     */
     public static void println(ANSIColour colour, String str) {
         print(colour, str + "\n");
     }
 
+
+    /**
+     * Prints a string using a string format with a given colour
+     * @param colour The colour to use
+     * @param format The string format to use
+     * @param args The arguments to pass into the string format
+     */
     public static void printf(ANSIColour colour, String format, Object ...args) {
         print(colour, String.format(format, args));
     }
 
+    /**
+     * Returns the string represented with the colour provided
+     * @param colour The colour to use
+     * @param str The string to print
+     * @return The coloured string
+     */
     public static String colourString(ANSIColour colour, String str) {
         return colour.ansiCode + str + ANSIColour.RESET.ansiCode;
     }
--- Calculator_And_String_Analyzer/Tester.java
+++ Calculator_And_String_Analyzer/Tester.java
@@ -1,4 +1,11 @@
 public class Tester {
+    /** 
+     * Compares two values and checks whether they are equal. Prints a message if the test failed. 
+     * 
+     * @param expectedValue The expected value
+     * @param receivedValue The received value
+     * @param messageIfFailed The message to print if the test fails
+     * */
     public static <T> boolean test(T expectedValue, T receivedValue, String messageIfFailed) {
         if(!expectedValue.equals(receivedValue)) {
             System.out.printf("%s - Expected Value: '%s' | Received Value: '%s'\n", messageIfFailed, expectedValue, receivedValue);
