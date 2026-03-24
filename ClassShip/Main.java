e9656baae7585027b5155f54b6d9965fa54ce8e1
TVA018 <thomas.vu018@gmail.com>
2026-03-24 14:52:16 -0400

    Patch

--- ClassShip/Main.java
+++ ClassShip/Main.java
@@ -9,6 +9,7 @@ public class Main {
         String startingPosStr = scanner.readLine("Where would you like to start? (-25 - +25, Leave blank to start at the default\n> ");
         Ship ship;
 
+        // Creating the ship
         try {
             int startingPos = Integer.parseInt(startingPosStr);
 
@@ -21,6 +22,7 @@ public class Main {
             ship = new Ship();
         }
 
+        // Give a simple rundown of the game
         TerminalTextFormatter.println("-->SHIP GAME<--", ANSIFlag.BOLD);
         TerminalTextFormatter.println(
             String.format("Try to reach %s by answering these questions correctly!", Ship.MAX_POSITION), 
@@ -29,18 +31,20 @@ public class Main {
         TerminalTextFormatter.println("NOTE: DIVISION QUESTIONS TRUNCATE THE DECIMALS", ANSIFlag.YELLOW_TEXT);
 
         System.out.println("PRESS ENTER TO CONTINUE");
-        scanner.readLine();
+        scanner.readLine(); // Wait for user input so they have time to read instructions
 
+        // Game loop
+        System.out.println(ship);
         while (ship.getPosition() < Ship.MAX_POSITION) {
-            System.out.println(ship);
-
+            // Move forward or backwards based on the user's answer
             if(MathQuestionaire.askQuestion(scanner))
                 ship.moveForward();
             else
                 ship.moveBackward();
-        }
 
-        System.out.println(ship);
+            // Re-render ship
+            System.out.println(ship);
+        }
         
         TerminalTextFormatter.println("CONGRATULATIONS, YOU WIN!", ANSIFlag.BOLD, ANSIFlag.YELLOW_TEXT);
 
